3a4241671b69c9732308552e70f81ea735e017ff
Hugo Dieb <hugodieb.hd@gmail.com>
2016-05-02 22:56:30 -0300

    implementacao envio de email.

---
+++ Email/Email/main.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.btnClean = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.txtfrom = New System.Windows.Forms.TextBox()
+        Me.txtpass = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtto = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtsubject = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.rtxdescription = New System.Windows.Forms.RichTextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.rtxdescription)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtpass)
+        Me.GroupBox1.Controls.Add(Me.txtsubject)
+        Me.GroupBox1.Controls.Add(Me.txtto)
+        Me.GroupBox1.Controls.Add(Me.txtfrom)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.White
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(482, 326)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'btnSend
+        '
+        Me.btnSend.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnSend.Location = New System.Drawing.Point(150, 348)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(111, 23)
+        Me.btnSend.TabIndex = 1
+        Me.btnSend.Text = "Enviar..."
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'btnClean
+        '
+        Me.btnClean.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnClean.Location = New System.Drawing.Point(267, 348)
+        Me.btnClean.Name = "btnClean"
+        Me.btnClean.Size = New System.Drawing.Size(111, 23)
+        Me.btnClean.TabIndex = 1
+        Me.btnClean.Text = "Limpar..."
+        Me.btnClean.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnCancel.Location = New System.Drawing.Point(384, 348)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(111, 23)
+        Me.btnCancel.TabIndex = 1
+        Me.btnCancel.Text = "Cancelar..."
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'txtfrom
+        '
+        Me.txtfrom.Location = New System.Drawing.Point(37, 46)
+        Me.txtfrom.Name = "txtfrom"
+        Me.txtfrom.Size = New System.Drawing.Size(184, 23)
+        Me.txtfrom.TabIndex = 0
+        '
+        'txtpass
+        '
+        Me.txtpass.Location = New System.Drawing.Point(262, 46)
+        Me.txtpass.Name = "txtpass"
+        Me.txtpass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtpass.Size = New System.Drawing.Size(184, 23)
+        Me.txtpass.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(34, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "De..."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(259, 28)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Senha..."
+        '
+        'txtto
+        '
+        Me.txtto.Location = New System.Drawing.Point(37, 92)
+        Me.txtto.Name = "txtto"
+        Me.txtto.Size = New System.Drawing.Size(226, 23)
+        Me.txtto.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(34, 74)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 15)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Para..."
+        '
+        'txtsubject
+        '
+        Me.txtsubject.Location = New System.Drawing.Point(37, 137)
+        Me.txtsubject.Name = "txtsubject"
+        Me.txtsubject.Size = New System.Drawing.Size(226, 23)
+        Me.txtsubject.TabIndex = 0
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.Color.Black
+        Me.Label4.Location = New System.Drawing.Point(34, 119)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 15)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Assunto..."
+        '
+        'rtxdescription
+        '
+        Me.rtxdescription.Location = New System.Drawing.Point(37, 180)
+        Me.rtxdescription.Name = "rtxdescription"
+        Me.rtxdescription.Size = New System.Drawing.Size(409, 130)
+        Me.rtxdescription.TabIndex = 2
+        Me.rtxdescription.Text = ""
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.Black
+        Me.Label5.Location = New System.Drawing.Point(34, 162)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 15)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Descrição..."
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(505, 381)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnClean)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Font = New System.Drawing.Font("Siemens Sans SC Black", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.Control
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Enviar Email"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnSend As System.Windows.Forms.Button
+    Friend WithEvents btnClean As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents rtxdescription As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtpass As System.Windows.Forms.TextBox
+    Friend WithEvents txtsubject As System.Windows.Forms.TextBox
+    Friend WithEvents txtto As System.Windows.Forms.TextBox
+    Friend WithEvents txtfrom As System.Windows.Forms.TextBox
+
+End Class
---
+++ Email/Email/main.vb
@@ -0,0 +1,23 @@
+﻿Public Class main
+
+    Public Sub senderMail()
+
+    End Sub
+
+    Public Sub cleanBox()
+
+    End Sub
+
+    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+
+    End Sub
+
+    Private Sub btnClean_Click(sender As Object, e As EventArgs) Handles btnClean.Click
+        cleanBox()
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Me.Close()
+
+    End Sub
+End Class
